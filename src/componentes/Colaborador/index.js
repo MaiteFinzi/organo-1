885dd4ace8d8e5113a4ec13fcdf7eeb70c329068
Maite Finzi de Sousa Buiatti <maitefinzi@gmail.com>
2024-06-03 13:51:22 -0300

    alteraçoes realizadas durante o curso

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,40 @@
-import './colaborador.css'
+import { IoMdCloseCircle } from "react-icons/io";
+import { CiHeart } from "react-icons/ci";
+import { FaHeart } from "react-icons/fa";
+import './colaborador.css';
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
+
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar() {
+        aoFavoritar(colaborador.id);
+    }
+
+    const propsfavorito = {
+        size:25,
+        onClick: favoritar
+    }
+    
     return (<div className="colaborador">
+        <IoMdCloseCircle 
+            size={25} 
+            className="deletar" 
+            onClick={() => aoDeletar(colaborador.id)} /*para o react entender que tem que apagar o card so depois do click*/ 
+        />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className="favorito">
+                {colaborador.favorito 
+                    ? <FaHeart {...propsfavorito} color='#FF0000'/> 
+                    : <CiHeart {...propsfavorito} />
+                }
+            </div>
         </div>
     </div>)
 }
 
 export default Colaborador
+
